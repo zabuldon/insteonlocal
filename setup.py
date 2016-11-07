92c9e8320fadb2adac9e37b13a16b9a468eaac67
Michael Long <mlong@mlong.us>
2016-11-06 23:53:01 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+desc = """\
+InsteonLocal allows local (non-cloud) control of the Insteon Hub 2245-222
+"""
+
+setup(name='insteonlocal',
+      version='0.1',
+      py_modules=['insteonlocal'],
+      author='phareous',
+      author_email='mplong@gmail.com',
+      url='https://github.com/phareous/insteonlocal',
+      long_description=desc
+      )
