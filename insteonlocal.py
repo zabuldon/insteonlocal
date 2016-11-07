f8f58e338ab30a8e7c9f23de347e8c033b19969e
Michael Long <mlong@mlong.us>
2016-11-07 00:35:38 -0500

    packaging

--- insteonlocal.py
+++ insteonlocal.py
@@ -1,6 +1,18 @@
-#from urllib import urlencode
 import requests, time, pprint, logging, logging.handlers, sys, json
 from collections import OrderedDict
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 # Todo:
 # error handling
@@ -10,7 +22,6 @@ from collections import OrderedDict
 # switch on updates/broadcasts
 
 class Hub(object):
-
     def __init__(self, ip, username, password, port="25105", logfile="/tmp/insteonlocal.log", consoleLog = False):
         self.ip = ip
         self.username = username
--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from distutils.core import setup
 
-desc = """\
-InsteonLocal allows local (non-cloud) control of the Insteon Hub 2245-222
-"""
-
-setup(name='insteonlocal',
-      version='0.1',
-      py_modules=['insteonlocal'],
-      author='phareous',
-      author_email='mplong@gmail.com',
-      url='https://github.com/phareous/insteonlocal',
-      long_description=desc,
-      data_files=[('', ['device_models.json', 'device_categories.json'])],
-      requires=['requests', 'time', 'pprint', 'logging', 'logging.handlers', 'sys', 'json', 'collections'],
-      provides=['insteonlocal']
-      )
+setup(
+  name = 'insteonlocal',
+  py_modules = ['insteonlocal'],
+  version = '0.1',
+  description = 'InsteonLocal allows local (non-cloud) control of the Insteon Hub 2245-222',
+  author = 'Michael Long',
+  author_email = 'mplong@gmail.com',
+  url = 'https://github.com/phareous/insteonlocal',
+  download_url = 'https://github.com/phareous/insteonlocal/tarball/0.1',
+  keywords = ['insteon'],
+  data_files = [('', ['device_models.json', 'device_categories.json'])],
+  requires = ['requests', 'time', 'pprint', 'logging', 'logging.handlers', 'sys', 'json', 'collections'],
+  provides = ['insteonlocal']
+)
