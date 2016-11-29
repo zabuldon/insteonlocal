3da581878512bbc6568d4aa45751c451a3f0d015
Michael Long <mlong@mlong.us>
2016-11-28 21:16:45 -0500

    update package

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 #from distutils.core import setup
-from setuptools import setup
+from setuptools import setup, find_packages
 setup(
   name = 'insteonlocal',
   py_modules = ['insteonlocal'],
-  version = '0.26',
+  version = '0.27',
   description = 'InsteonLocal allows local (non-cloud) control of the Insteon Hub 2245-222',
   author = 'Michael Long',
   author_email = 'mplong@gmail.com',
   url = 'https://github.com/phareous/insteonlocal',
   download_url = 'https://github.com/phareous/insteonlocal/tarball/0.21',
   keywords = ['insteon'],
-  package_data = {'insteonlocal': ['data/*']},
+  package_data = {'': ['data/*.json']},
   requires = ['requests', 'time', 'pprint', 'logging', 'logging.handlers', 'sys', 'json', 'collections'],
   provides = ['insteonlocal'],
+  install_requires = [],
+  packages=find_packages(exclude=['tests', 'tests.*']),
   include_package_data=True, # use MANIFEST.in during install
-#  packages=setup.find.packages(),
   zip_safe=False
 )
