a6797d7bacbad5f5881409da428c65338ae7cc5e
Michael Long <mlong@mlong.us>
2016-12-18 22:30:08 -0500

    merged switch text change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'insteonlocal',
   py_modules = ['insteonlocal'],
-  version = '0.33',
+  version = '0.34',
   description = 'InsteonLocal allows local (non-cloud) control of the Insteon Hub 2245-222',
   author = 'Michael Long',
   author_email = 'mplong@gmail.com',
